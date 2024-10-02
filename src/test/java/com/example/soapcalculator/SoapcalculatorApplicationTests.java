0ba4bf66d30c026c538e1b473f5808e77f016981
Dicle Yenigün <33228482+dicleyenigun@users.noreply.github.com>
2024-10-02 08:11:10 +0300

    Initial commit

---
+++ src/test/java/com/example/soapcalculator/SoapcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soapcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapcalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
