c63ef504e9e6b22de85adbef8fcf0ffecfbb88ed
Dicle Yenigün <33228482+dicleyenigun@users.noreply.github.com>
2024-10-02 08:51:06 +0300

    Added PayloadRoot and ResponsePayload annotations

--- src/main/java/com/example/soapcalculator/CalculatorEndpoint.java
+++ src/main/java/com/example/soapcalculator/CalculatorEndpoint.java
@@ -1,9 +1,15 @@
 package com.example.soapcalculator;
 
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 @Endpoint
 public class CalculatorEndpoint {
 
     private static final String NAMESPACE_URL = "http://example.com/democalculator/wsdl";
+
+    @PayloadRoot(namespace = NAMESPACE_URL, localPart = "AddRequest")
+    @ResponsePayload
+
 }
