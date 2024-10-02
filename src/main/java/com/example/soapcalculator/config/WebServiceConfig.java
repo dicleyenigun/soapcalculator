2da3d3cbb32577d39e6068a93ce9ba746ae2025a
Dicle Yenigün <33228482+dicleyenigun@users.noreply.github.com>
2024-10-02 09:00:48 +0300

    annotations entered

--- src/main/java/com/example/soapcalculator/config/WebServiceConfig.java
+++ src/main/java/com/example/soapcalculator/config/WebServiceConfig.java
@@ -1,4 +1,10 @@
 package com.example.soapcalculator.config;
 
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ws.config.annotation.EnableWs;
+
+@EnableWs
+@Configuration
 public class WebServiceConfig {
 }
