8cc8bc09c709fc75567eb06e7c017f3c6bfa2270
Dicle Yenigün <33228482+dicleyenigun@users.noreply.github.com>
2024-10-02 09:04:49 +0300

    DefaultWsdl11Definition bean defined

--- src/main/java/com/example/soapcalculator/config/WebServiceConfig.java
+++ src/main/java/com/example/soapcalculator/config/WebServiceConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.XsdSchema;
 
 @EnableWs
 @Configuration
@@ -20,5 +22,16 @@ public class WebServiceConfig {
         servlet.setTransformWsdlLocations(true);
 
         return new ServletRegistrationBean<>(servlet, "/ws/*");
+    }
+
+    @Bean(name = "democalculator")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema democalculatorSchema){
+        DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
+        definition.setPortTypeName("DemocalculatorPort");
+        definition.setLocationUri("/ws");
+        definition.setTargetNamespace("http://example.com/democalculator");
+        definition.setSchema(democalculatorSchema);
+
+        return definition;
     }
 }
