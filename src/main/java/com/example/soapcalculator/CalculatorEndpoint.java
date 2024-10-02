aed8076d8f3ac83e3526ee54831f400d04001e5c
Dicle Yenigün <33228482+dicleyenigun@users.noreply.github.com>
2024-10-02 08:48:39 +0300

    Defining a URL used for sharing data with SOAP web services

--- src/main/java/com/example/soapcalculator/CalculatorEndpoint.java
+++ src/main/java/com/example/soapcalculator/CalculatorEndpoint.java
@@ -4,4 +4,6 @@ import org.springframework.ws.server.endpoint.annotation.Endpoint;
 
 @Endpoint
 public class CalculatorEndpoint {
+
+    private static final String NAMESPACE_URL = "http://example.com/democalculator/wsdl";
 }
