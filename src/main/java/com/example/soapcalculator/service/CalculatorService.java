51712c9c55b721aa6d240339a8cd39cb37fdd70d
Dicle Yenigün <33228482+dicleyenigun@users.noreply.github.com>
2024-10-02 08:33:00 +0300

    Addition was done by taking two integers and sending them to a SOAP web service

--- src/main/java/com/example/soapcalculator/service/CalculatorService.java
+++ src/main/java/com/example/soapcalculator/service/CalculatorService.java
@@ -1,5 +1,7 @@
 package com.example.soapcalculator.service;
 
+import com.example.soapcalculator.wsdl.AddRequest;
+import com.example.soapcalculator.wsdl.AddResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
@@ -8,4 +10,15 @@ public class CalculatorService extends WebServiceGatewaySupport {
 
     private static final String SOAP_URL = "http://www.dneonline.com/calculator.asmx";
 
+    public int add(int intA, int intB){
+
+        AddRequest request = new AddRequest();
+        request.setIntA(intA);
+        request.setIntB(intB);
+
+        AddResponse response = (AddResponse) getWebServiceTemplate()
+                .marshalSendAndReceive(SOAP_URL, request);
+
+        return response.getAddResult();
+    }
 }
